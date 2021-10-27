193cbcd5b59a86605542a836c194ff7798292510
Adrian Jendo <ajendo@uwaterloo.ca>
2021-10-26 22:10:25 -0700

    added bin2decimal and decimal2binary conversions

--- baseConverters/main.py
+++ baseConverters/main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 # Use
 #   alias factorTree='cd ~ && cd rootdir-scripts && cd factorTree && f(){pipenv run python3 factorTree.py "$1"; unset -f f; cd ~;}; f'
@@ -6,21 +7,64 @@ import sys
 
 
 if __name__ == "__main__":
-    # argv[1] will be number
-    # argv[2] will be base
-    # argv[3] will be convert to
+    args = sys.argv
 
-    # num = int(sys.argv[1])
-    # args = sys.argv
+    if len(args) < 3:
+        print("Need at least 2 operators")
+    elif len(args) > 4:
+        print("Yo too many params")
 
-    # if len(args) < 3:
-    #     print("Need at least 2 operators")
-    # else:
-    #     numArgs = args[1:]
-    #     sum = 0
-    #     for arg in numArgs:
-    #         temp = float(arg)
+    fromBase = sys.argv[1]
+    toBase = sys.argv[3]
+    num = sys.argv[2]
 
-    print("x")
+    if (fromBase == "10" or fromBase == "decimal") and (
+        toBase == "2" or toBase == "binary"
+    ):
+        resp = ""
+        num = int(num)  # cast to int
+        # Padding
+        numDigits = math.log2(num) // 1 + 1
+        padding = ""
+        while numDigits % 4 != 0:
+            padding += "0"
+            numDigits += 1
+        while num > 0:
+            bit = num % 2
+            resp = "1" + resp if bit == 1 else "0" + resp
+            num = num // 2
 
-    print("y")
+        count = 0
+        resp = padding + resp
+        for char in resp:
+            count += 1
+            if count % 4 == 0:
+                print(char, end=" ")
+            else:
+                print(char, end="")
+        print()
+    elif (fromBase == "10" or fromBase == "decimal") and (
+        toBase == "16" or toBase == "hex"
+    ):
+        print("bin2dec")
+    elif (fromBase == "2" or fromBase == "binary") and (
+        toBase == "10" or toBase == "decimal"
+    ):
+        resp = 0
+        power = 0
+        for i in range(len(num) - 1, -1, -1):
+            resp += int(num[i]) * pow(2, power)
+            power += 1
+        print(resp)
+    elif (fromBase == "2" or fromBase == "binary") and (
+        toBase == "16" or toBase == "hex"
+    ):
+        print("bin2hex")
+    elif (fromBase == "16" or fromBase == "hex") and (
+        toBase == "2" or toBase == "binary"
+    ):
+        print("hex2bin")
+    elif (fromBase == "16" or fromBase == "hex") and (
+        toBase == "10" or toBase == "decimal"
+    ):
+        print("bin2hex")
